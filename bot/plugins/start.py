aef268c5b2b1868c064f6e6168a650eb4bfd94c5
vortexcyba <88479442+vortexcyba@users.noreply.github.com>
2021-11-24 01:49:44 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,25 +1,67 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from bot.utils import Utilities
+from bot.config import Config
 from ..screenshotbot import ScreenShotBot
 
 
 @ScreenShotBot.on_message(filters.private & filters.command("start"))
-async def start(c, m):
+async def start(c, m, cb=False):
+    owner_id = Config.AUTH_USERS[0]
+    username = 'imjerin'
+    mention = '[Jerin](https://t.me/imjerin)'
+    try:
+        owner = await c.get_users(owner_id)
+        username = owner.username if owner.username else 'Ns_AnoNymous'
+        mention = owner.mention(style="md")
+    except Exception as e:
+        print(e)
 
-    await m.reply_text(
-        text=f"**Hi there {m.from_user.mention}.\n\nI'm Screenshot Generator. I can provide screenshots from**"
-        "your video files almost instantly without downloading the entire file. For more details check /help.**",
-        quote=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "👤 Maintained By", url="https://t.me/imjerin"),
-                    InlineKeyboardButton(
-                        "⚙️ Settings", callback_data="set+settings")
-                ]
-            ]
-         )
-    )
+    BUTTONS = [[
+        InlineKeyboardButton("Maintained By 👤", url=f"https://t.me/{username}")
+        ],[
+        InlineKeyboardButton("Help ⁉️", callback_data="help"),
+        InlineKeyboardButton("Settings ⚙", callback_data="set+settings")
+        ],[
+        InlineKeyboardButton("Close 📛", callback_data="close")
+    ]]
+
+    TEXT = f"👋 Hi {m.from_user.mention},\n\nI'm Screenshot Generator Bot. I can provide screenshots, sample video from "
+    TEXT += "your video files and also can trim. For more details check help.\n\n"
+    TEXT += f"**Maintained By:** {mention}"
+
+    if cb:
+        try:
+            await m.message.edit(
+                text=TEXT,
+                reply_markup=InlineKeyboardMarkup(BUTTONS)
+            )
+        except:
+            pass
+    else:
+        await m.reply_text(
+            text=TEXT,
+            quote=True,
+            reply_markup=InlineKeyboardMarkup(BUTTONS)
+        )
+
+
+# i generally liked to use regex filters for callback 
+# but since odysseusmax used lambda i am also using the same
+@ScreenShotBot.on_callback_query(
+    filters.create(lambda _, __, query: query.data.startswith("home"))
+)
+async def home_cb(c, m):
+    await m.answer()
+    await start(c, m, True)
+
+
+@ScreenShotBot.on_callback_query(
+    filters.create(lambda _, __, query: query.data.startswith("close"))
+)
+async def close_cb(c, m):
+    try:
+        await m.message.delete()
+        await m.message.reply_to_message.delete()
+    except:
+        pass
